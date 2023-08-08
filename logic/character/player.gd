027af13c163795853f9a08cb240383b45f7d0b27
arfitale <arfitale@gmail.com>
2023-08-09 02:51:14 +0700

    add jump to player

--- logic/character/player.gd
+++ logic/character/player.gd
@@ -3,7 +3,7 @@ class_name Player
 
 var gravity: float = 980.0
 var speed: float = 200.0
-var jump_impulse: float = 200.0
+var jump_impulse: float = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
 
@@ -16,7 +16,7 @@ func _physics_process(delta):
 	# apply gravity
 	if !is_on_floor():
 		velocity.y += gravity * delta
-		
+	
 	# Get direction by player input
 	direction.x = Input.get_axis('move_left', 'move_right')
 	if direction.x != 0:
@@ -26,7 +26,15 @@ func _physics_process(delta):
 		animated_sprite.flip_h = !isFaceRight(direction.x)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-	move_and_slide()
 	
-func isFaceRight(horizon_direction: int) -> bool:
-	return true if horizon_direction > 0 else false 
+	# handle jump
+	if is_on_floor() && Input.is_action_pressed("jump"):
+		jump(jump_impulse) 
+	
+	move_and_slide()
+
+func jump(_jump_impulse: float) -> void:
+	velocity.y += _jump_impulse
+
+func isFaceRight(_horizon_direction: int) -> bool:
+	return true if _horizon_direction > 0 else false 
