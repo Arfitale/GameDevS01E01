3e385d9166db28ac0452aa49dc4178a67165db61
arfitale <arfitale@gmail.com>
2023-08-09 12:56:20 +0700

    switch to AnimationPlayer for player character

--- logic/character/player.gd
+++ logic/character/player.gd
@@ -5,12 +5,9 @@ var gravity: float = 980.0
 var speed: float = 200.0
 var jump_impulse: float = -300.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _ready():
-	animated_sprite.play("run")
-
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	var direction: Vector2 = Vector2.ZERO
 	
 	# apply gravity
@@ -22,8 +19,8 @@ func _physics_process(delta):
 	if direction.x != 0:
 		velocity.x = speed * direction.x
 		
-		# change sprite direction
-		animated_sprite.flip_h = !isFaceRight(direction.x)
+		# Handle run animation
+		handle_run_anim(direction.x)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
@@ -36,5 +33,10 @@ func _physics_process(delta):
 func jump(_jump_impulse: float) -> void:
 	velocity.y += _jump_impulse
 
-func isFaceRight(_horizon_direction: int) -> bool:
+func is_face_right(_horizon_direction: int) -> bool:
 	return true if _horizon_direction > 0 else false 
+
+func handle_run_anim(_horizon_direction: int) -> void:
+	if _horizon_direction > 0:
+		animation_player.play('run_right')
+	else: animation_player.play('run_left')
