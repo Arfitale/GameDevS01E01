f7297fb727db9f28e6955e57a7bb8cc7208fca74
arfitale <arfitale@gmail.com>
2023-08-11 14:37:14 +0700

    create jump feature for player

--- logic/character/player.gd
+++ logic/character/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 enum player_state {IDLE, RUN, JUMP, FALL}
+const state_debug = {0: 'Idle', 1: 'Run', 2: 'Jump', 3: 'Fall'}
 
 var gravity: float = 980.0
 var speed: float = 200.0
@@ -10,8 +11,10 @@ var jump_impulse: float = -300.0
 var state = player_state.RUN
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite
 
 func _physics_process(delta) -> void:
+#	print(state_debug[state])
 	match state:
 		player_state.IDLE: state_idle(delta)
 		player_state.RUN: state_run(delta)
@@ -35,7 +38,7 @@ func state_idle(delta: float) -> void:
 	
 	# Exit state to Jump state
 	if is_on_floor() && Input.is_action_pressed("jump"):
-		state = player_state.JUMP
+		jump()
 	
 	move_and_slide()
 
@@ -60,12 +63,34 @@ func state_run(delta: float) -> void:
 	
 	# Exit to Jump state
 	if is_on_floor() && Input.is_action_pressed("jump"):
-		state = player_state.JUMP
+		jump()
 	
 	move_and_slide()
 
 func state_jump(delta: float) -> void:
-	pass
+	var direction: Vector2 = Vector2.ZERO
+	direction.x = get_x_direction()
+	
+	# apply gravity
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	
+	if direction.x != 0:
+		velocity.x = speed * direction.x
+		set_sprite_face(is_face_right(direction.x))
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, speed)
+	
+	move_and_slide()
+	
+	print(velocity.y)
+	
+	if is_on_floor():
+		# Exit state to Idle when player doesn't move
+		if direction.x == 0:
+			state = player_state.IDLE
+		else:
+			state = player_state.RUN
 
 func state_fall(delta: float) -> void:
 	pass
@@ -84,5 +109,10 @@ func handle_run_anim(_horizon_direction: int) -> void:
 		animation_player.play('run_right')
 	else: animation_player.play('run_left')
 
-func handle_jump_anim() -> void:
+func jump() -> void:
+	velocity.y += jump_impulse
+	state = player_state.JUMP
 	animation_player.play('jump')
+
+func set_sprite_face(_is_face_right: bool = true) -> void:
+	sprite.flip_h = !_is_face_right
