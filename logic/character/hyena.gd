b93ded8a6d8616375294007ddbaaaf90ce2a5abf
arfitale <arfitale@gmail.com>
2023-09-24 10:10:24 +0700

    Add Hyena idle state
    Rebase from remote

--- logic/character/hyena.gd
+++ logic/character/hyena.gd
@@ -1,20 +1,20 @@
 extends CharacterBody2D
 
+enum hyena_state {IDLE, CHASE, WANDER}
+
 var move_speed: float = 250.0
 var gravity: float = 980.0
 var move_dir: int = 1
 
+# Hyena stats
+var _state = hyena_state.IDLE
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed('TestKey'):
-		move_dir = move_dir * -1
-		sprite.flip_h = true if move_dir > 0 else false
-		
-
 func _physics_process(delta: float) -> void:
-	animation_player.play('run')
-	velocity.x = move_speed * move_dir
-	velocity.y += gravity * delta
-	move_and_slide()
+	match _state:
+		hyena_state.IDLE: state_idle(delta)
+
+func state_idle(_delta: float) -> void:
+	animation_player.play('idle')
