833b1f8482846e5e41d2ac89de9a92e0bde036eb
arfitale <arfitale@gmail.com>
2023-10-18 20:48:29 +0700

    add attack sensor logic

--- logic/character/hyena.gd
+++ logic/character/hyena.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 signal player_detected
 
-enum hyena_state {IDLE, CHASE, WANDER}
+enum hyena_state {IDLE, CHASE, WANDER, ATTACK}
 
-var move_speed: float = 250.0
+var move_speed: float = 150.0
 var gravity: float = 980.0
 
 # Hyena stats
 var _state = hyena_state.IDLE
-var detector_data: Dictionary = {'playerSensor': null}
+var detector_data: Dictionary = {'playerSensor': null, 'attackSensor': null}
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite
@@ -18,6 +18,7 @@ func _physics_process(delta: float) -> void:
 	match _state:
 		hyena_state.IDLE: state_idle(delta)
 		hyena_state.CHASE: state_chase(delta)
+		hyena_state.ATTACK: state_attack(delta)
 
 func state_idle(_delta: float) -> void:
 	animation_player.play('idle')
@@ -33,7 +34,6 @@ func state_chase(_delta: float) -> void:
 		var _distance: float = self.global_position.distance_to(_player.global_position)
 		var _direction: float = round(self.global_position.direction_to(_player.global_position).x)
 		
-		
 		if _distance > 32:
 			velocity.x = move_speed * _direction
 			animation_player.play('run')
@@ -44,13 +44,35 @@ func state_chase(_delta: float) -> void:
 	velocity.y += gravity * _delta
 	move_and_slide()
 
+func state_attack(_delta: float) -> void:
+	animation_player.play('idle')
+	
+	velocity.x = move_toward(velocity.x, 0, move_speed)
+	velocity.y += gravity * _delta
+	move_and_slide()
+	# Exit state
+	if !detector_data['attackSensor']:
+		if detector_data['playerSensor']:
+			_state = hyena_state.CHASE
+		else:
+			_state = hyena_state.IDLE
+
 func on_entity_detected(body: Node2D) -> void:
 	if body is Player:
 		detector_data['playerSensor'] = body
 		_state = hyena_state.CHASE
 
-
 func on_entity_exited(body: Node2D) -> void:
 	if body is Player:
 		detector_data['playerSensor'] = null
 		_state = hyena_state.IDLE
+
+
+func _on_attack_sensor_body_entered(body: Node2D) -> void:
+	if body is Player:
+		detector_data['attackSensor'] = body
+		_state = hyena_state.ATTACK
+
+func _on_attack_sensor_body_exited(body: Node2D) -> void:
+	if body is Player:
+		detector_data['attackSensor'] = null
