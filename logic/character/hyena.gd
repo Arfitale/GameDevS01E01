0f4a43309590fd000ef4877dcabef9da86919fa5
arfitale <arfitale@gmail.com>
2023-10-25 12:31:34 +0700

    add Hyena chase delay and basic attack delay

--- logic/character/hyena.gd
+++ logic/character/hyena.gd
@@ -13,6 +13,8 @@ var detector_data: Dictionary = {'playerSensor': null, 'attackSensor': null}
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite
+@onready var chase_delay_timer: Timer = $Timers/ChaseDelayTimer
+@onready var basic_attack_delay_timer: Timer = $Timers/BasicAttackDelayTimer
 
 func _physics_process(delta: float) -> void:
 	match _state:
@@ -42,14 +44,17 @@ func state_chase(_delta: float) -> void:
 func state_attack(_delta: float) -> void:
 	var _player: CharacterBody2D = detector_data['attackSensor']
 	
-	if sprite.flip_h:
-		animation_player.play('basic_attack_right')
-	else:
-		animation_player.play('basic_attack_left')
+	if basic_attack_delay_timer.is_stopped():
+		if sprite.flip_h:
+			animation_player.play('basic_attack_right')
+		else:
+			animation_player.play('basic_attack_left')
+		basic_attack_delay_timer.start()
 	
-	velocity.x = move_toward(velocity.x, 0, move_speed)
+	velocity.x = Vector2.ZERO.x
 	velocity.y += gravity * _delta
 	move_and_slide()
+	
 	# Exit state
 	if !detector_data['attackSensor']:
 		if detector_data['playerSensor']:
@@ -60,13 +65,12 @@ func state_attack(_delta: float) -> void:
 func on_entity_detected(body: Node2D) -> void:
 	if body is Player:
 		detector_data['playerSensor'] = body
-		_state = hyena_state.CHASE
+		chase_delay_timer.start()
 
 func on_entity_exited(body: Node2D) -> void:
 	if body is Player:
 		detector_data['playerSensor'] = null
 		_state = hyena_state.IDLE
-
 
 func _on_attack_sensor_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -76,3 +80,8 @@ func _on_attack_sensor_body_entered(body: Node2D) -> void:
 func _on_attack_sensor_body_exited(body: Node2D) -> void:
 	if body is Player:
 		detector_data['attackSensor'] = null
+
+# When chase timer is zero then Hyena chase player
+func _on_chase_delay_timer_timeout() -> void:
+	if detector_data['playerSensor']:
+		_state = hyena_state.CHASE
