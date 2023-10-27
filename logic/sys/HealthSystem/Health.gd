31a7ac990e633234e4f9319e4f795db8801076a0
arfitale <arfitale@gmail.com>
2023-10-27 08:03:47 +0700

    add basic healthbar UI and health logic

---
+++ logic/sys/HealthSystem/Health.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+signal max_health_changed
+signal changed
+signal depleted
+
+@export var max_health: int = 10 : set = set_max_health
+@onready var current_hp: int = max_health : set = set_current_hp
+
+func _ready() -> void:
+	emit_signal('max_health_changed', max_health)
+	emit_signal('changed', current_hp)
+
+func set_max_health(new_max: int) -> void:
+	max_health = new_max
+	max_health = max(1, max_health)
+	emit_signal('max_health_changed', max_health)
+
+func set_current_hp(new_hp: int) -> void:
+	current_hp = new_hp
+	current_hp = clamp(current_hp, 0, max_health)
+	emit_signal('changed', current_hp)
+	
+	if current_hp == 0:
+		emit_signal('depleted')
